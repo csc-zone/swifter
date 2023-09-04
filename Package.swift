ad30df64f15ec2a95f59191524b1f5e4c31ba42b
csc-zone <csc-zone@gmail.com>
2023-09-05 04:57:56 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "Swifter",
 
-  platforms: [.macOS(.v10_15)],
+  platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16),],
 
   products: [
     .library(name: "Swifter", targets: ["Swifter"]),
